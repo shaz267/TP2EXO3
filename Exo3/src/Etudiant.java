d407fbe261b68fcea37c8669e984ba50ec3ae589
Marin <bourdonmarin@gmail.com>
2023-09-25 11:25:57 +0200

    ajout calcul moyenne

--- Exo3/src/Etudiant.java
+++ Exo3/src/Etudiant.java
@@ -19,14 +19,15 @@ public class Etudiant {
      * @param mat
      * @param note
      */
-    public void ajouterNotes(Matiere mat, double note){
+    public boolean ajouterNotes(Matiere mat, double note){
         Matiere m = mat;
+        boolean res = false;
         //Vérifie si la matière est déjà dans les matières de l'etudiant
         if(resultats.containsKey(m)){
             //Si la note est située entre 0 et 20 alors on ajoute la note à la liste des notes
             if (note <= 20 && note >= 0){
-                List<Double> notesMatieres = resultats.get(m);
-                notesMatieres.add(note);
+                resultats.get(m).add(note);
+                res = true;
             }
             //Si la matière n'est pas dans resultats mais qu'il est bien dans la formation de l'etudiant
         } else if(formation.getMatieres().containsKey(m)){
@@ -35,12 +36,29 @@ public class Etudiant {
                 List<Double> notesMatieres = new ArrayList<>();
                 notesMatieres.add(note);
                 resultats.put(m,notesMatieres);
+                res = true;
             }
         }
+        return res;
     }
 
-    public double calculMoyenne(){
+    public double calculMoyenne(Matiere m){
         double resultat = 0;
 
+        // On parcourt la liste pour effectuer notre moyenne
+        for(int i = 0; i < resultats.get(m).size(); i++){
+            resultat += resultats.get(m).get(i);
+        }
+        // Enfin, on finit le calcul
+        resultat = resultat / resultats.get(m).size();
+        return resultat;
     }
+
+    /*public double calculMoyenneG(){
+        double resultat = 0;
+
+        double coeff = 0;
+
+
+    }*/
 }
